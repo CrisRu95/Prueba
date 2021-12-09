0d7e89d7be639d818a883ca5210e306b07c3ecef
CrisRu95 <46592084+CrisRu95@users.noreply.github.com>
2021-12-09 18:17:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ import someFunctions
 
 def main(): 
     print("Hello World")
-    print("Hola este es el mismo proyecto")
-    n1 = int(input("Dame unnum: "))
-    n2 = int(input("Dame otro num: "))
-    r = someFunctions.suma(n1, n2)
 if __name__ == "__main__": 
     main()
     

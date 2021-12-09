ae320b5e9f975f20d39fb68e8481e6006fd6a9ed
CrisRu95 <ecrisru@alumni.uv.es>
2021-12-09 18:04:14 +0100

    hecha la segunda bracnh

--- main.py
+++ main.py
@@ -3,11 +3,14 @@
 
 
 """This is the main function"""
-
+import someFunctions
 
 def main(): 
     print("Hello World")
-
+    print("Hola este es el mismo proyecto")
+    n1 = int(input("Dame unnum: "))
+    n2 = int(input("Dame otro num: "))
+    r = someFunctions.suma(n1, n2)
 if __name__ == "__main__": 
     main()
     
